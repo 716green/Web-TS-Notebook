9835b3afdaea47d6745f9d08fa9e7ba459100bb2
Bob Bass <bob@bbass.co>
2021-08-25 16:33:00 -0400

    finished redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 // todo - re-enable
 
 // import CodeCell from './components/code-cell';
+// import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        {/* <CodeCell /> */}
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
