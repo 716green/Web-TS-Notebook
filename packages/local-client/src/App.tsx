44335d597cfce384dd58674e9ec24063cea8de00
Bob Bass <bob@bbass.co>
2021-09-01 16:33:12 -0400

    issue with proxy

--- packages/local-client/src/App.tsx
+++ packages/local-client/src/App.tsx
@@ -1,7 +1,3 @@
-// todo - re-enable
-
-// import CodeCell from './components/code-cell';
-// import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
